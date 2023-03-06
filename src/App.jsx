d9e75659c6c7e589b67041e1fa53c9c08ae0af94
Juan Sanchez <juansan1993@hotmail.com>
2023-03-06 08:56:31 -0500

    Add function component to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
-// import { Counter } from './Counter';
+import { Counter } from './Counter';
 import { CounterFunction } from './CounterFunction';
 
 function App() {
-  // return <Counter initialCount={0} />;
-  return <CounterFunction />;
+  return (
+    <>
+      <Counter initialCount={0} />
+      <br />
+      <CounterFunction />
+    </>
+  );
 }
 
 export default App;
